641c53b8c49006fbfbd0af3a4f8b76a3894dbbc4
bishop254 <karanjachiuri@gmail.com>
2022-04-23 15:49:06 +0300

    Added a  component to display the Pentagon or Triangle Image.

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import styles from "../styles/Home.module.css";
 import { useState } from "react";
 import Rules from "../Components/Rules";
 import Header from "../Components/Header";
+import Game from "../Components/Game";
 
 export default function Home() {
   const [mode, setMode] = useState(false); //Toggle the mode
@@ -19,6 +20,7 @@ export default function Home() {
         <title>Rock Paper Scissors</title>
       </Head>
       <Header mode={mode} />
+      <Game mode={mode} />
       <footer>
         <div onClick={changeMode}>{mode ? "ADVANCED" : "NORMAL"} MODE</div>
         <div onClick={() => setModal(!modal)}>RULES</div>
