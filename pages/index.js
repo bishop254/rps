043de22ef6d49790e1b56072dee7c352cb86b304
bishop254 <karanjachiuri@gmail.com>
2022-04-23 11:20:23 +0300

    Added the Header Component to the main page.

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { useState } from "react";
 import Rules from "../Components/Rules";
+import Header from "../Components/Header";
 
 export default function Home() {
   const [mode, setMode] = useState(false); //Toggle the mode
@@ -14,6 +15,10 @@ export default function Home() {
 
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Rock Paper Scissors</title>
+      </Head>
+      <Header mode={mode} />
       <footer>
         <div onClick={changeMode}>{mode ? "ADVANCED" : "NORMAL"} MODE</div>
         <div onClick={() => setModal(!modal)}>RULES</div>
