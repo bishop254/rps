830d47bea0485171aa7927a64892bb46648d2d47
bishop254 <karanjachiuri@gmail.com>
2022-04-24 21:41:48 +0300

    Added logic when an option is picked by the User.

--- Components/GameMode.js
+++ Components/GameMode.js
@@ -1,17 +1,88 @@
 import Image from "next/image";
-import React from "react";
+import React, { useContext } from "react";
 import styles from "../styles/Game.module.css";
-    
+import { ContProvider } from "../Context/Context";
+
+const SELECTIONS = [
+  {
+    name: "rock",
+    beats: ["scissors", "lizard"],
+  },
+  {
+    name: "scissors",
+    beats: ["paper", "lizard"],
+  },
+  {
+    name: "paper",
+    beats: ["rock", "spock"],
+  },
+  {
+    name: "lizard",
+    beats: ["spock", "paper"],
+  },
+  {
+    name: "spock",
+    beats: ["rock", "scissors"],
+  },
+];
 
 const GameMode = ({ mode }) => {
+  const { pickedChoices, setPickedChoices } = useContext(ContProvider);
+
   let gameOptions = ["scissors", "spock", "paper", "lizard", "rock"];
   let normObjs = gameOptions.filter(
     (option) => gameOptions.indexOf(option) % 2 === 0
   );
 
   const openPlay = (option) => {
-      console.log('Hello')
-  }
+    let mainDiv = document.querySelector(".Game_mainGame__Yidf_");
+    let playDiv = document.querySelector(".play");
+
+    let pickedOption = document.getElementById(option).id;
+    const selection = SELECTIONS.find(
+      (select) => select.name === pickedOption
+    );
+    let compPick = computerPick(mode);
+    let isUserWinner = isWinner(selection, compPick);
+    let isCompWinner = isWinner(compPick, selection);
+
+    if (isUserWinner) {
+      console.log(selection.name, "beats", compPick.name);
+      setPickedChoices({
+        user: pickedOption,
+        comp: compPick.name,
+        winner: true,
+        score: pickedChoices.score + 1,
+      });
+    } else if (isCompWinner) {
+      setPickedChoices({
+        user: pickedOption,
+        comp: compPick.name,
+        winner: false,
+        score: pickedChoices.score - 1,
+      });
+    } else {
+      setPickedChoices({
+        user: pickedOption,
+        comp: compPick.name,
+        winner: null,
+        score: pickedChoices.score,
+      });
+    }
+
+    playDiv.id = "";
+    mainDiv.id = "inactive";
+  };
+
+  const computerPick = (mod) => {
+    let choice = mod ? 5 : 3;
+    let randIndex = Math.floor(Math.random() * choice);
+    return SELECTIONS[randIndex];
+  };
+
+  const isWinner = (pick1, pick2) => {
+    return pick1.beats.includes(pick2.name);
+  };
 
   return (
     <div className={styles.gameModeContainer}>
