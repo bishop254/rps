66676385fdd0825478e3db15d9b0e29f525bbf16
bishop254 <karanjachiuri@gmail.com>
2022-04-24 22:07:50 +0300

    Added some comments for various functions.

--- Components/GameMode.js
+++ Components/GameMode.js
@@ -37,17 +37,17 @@ const GameMode = ({ mode }) => {
   const openPlay = (option) => {
     let mainDiv = document.querySelector(".Game_mainGame__Yidf_");
     let playDiv = document.querySelector(".play");
-
     let pickedOption = document.getElementById(option).id;
+
     const selection = SELECTIONS.find(
       (select) => select.name === pickedOption
     );
+
     let compPick = computerPick(mode);
     let isUserWinner = isWinner(selection, compPick);
     let isCompWinner = isWinner(compPick, selection);
 
     if (isUserWinner) {
-      console.log(selection.name, "beats", compPick.name);
       setPickedChoices({
         user: pickedOption,
         comp: compPick.name,
@@ -74,12 +74,15 @@ const GameMode = ({ mode }) => {
     mainDiv.id = "inactive";
   };
 
+
+  // Logic to simulate the computer pick which is randomly selected.
   const computerPick = (mod) => {
     let choice = mod ? 5 : 3;
     let randIndex = Math.floor(Math.random() * choice);
     return SELECTIONS[randIndex];
   };
 
+  // Returns true if pick1 beats pick2
   const isWinner = (pick1, pick2) => {
     return pick1.beats.includes(pick2.name);
   };
